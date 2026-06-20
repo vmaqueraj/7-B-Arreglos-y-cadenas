f1f113f7e67b77365fb75d68cdaa762b04b0f419
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-06-20 14:17:24 -0500

    Ejercicio C: Primer intento de estructura del ejercicio

---
+++ EjercicioC.cpp
@@ -0,0 +1,22 @@
+// Ejercicio C Frecuencias
+
+#include <iostream>
+using namespace std;
+
+int main () {
+	int p, n, f, A[n];
+	cin>>p>>n;
+	
+	for(int i=0; i<n; i++){
+		cin >> f;
+		A[i] = f;
+	}
+	
+	for(int i=0; i<p; i++){
+		
+	}
+	
+	cout << p[i] << "-" << cont[i]; 
+	
+	return 0;
+}
