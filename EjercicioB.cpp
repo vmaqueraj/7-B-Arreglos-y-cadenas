fe2bdf6287c928a8ea2f810634ae44ef9945ea1e
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-06-19 19:20:29 -0500

    Ejercicio B: Intento de estructura inicial

---
+++ EjercicioB.cpp
@@ -0,0 +1,27 @@
+// Ejercicio B Palindromos Coding Rush
+
+#include <iostream>
+using namespace std;
+
+int main () {
+	int n;
+	char palabra[80];
+	cin >> n;
+	
+	for(int i=0; i<n; i++){
+		cin >> palabra[];
+		palabra[] = palabra [i];
+	}
+	
+	for(int i=0; i<n; i++){
+		palabra [i] 
+		if(){
+			cout << "P";	
+		}
+		else {
+			cout << "NP";
+		}
+	}
+	
+	return 0;
+}
