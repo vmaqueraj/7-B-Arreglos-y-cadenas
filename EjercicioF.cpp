ab4e4f6093e59635ea5febac5cc325fbea7ab498
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-06-21 12:54:58 -0500

    Ejercicio F: Se añadio string, y se mejoro el inicio del for

--- EjercicioF.cpp
+++ EjercicioF.cpp
@@ -1,14 +1,17 @@
 // Ejercicio F Ordenando las letras de la línea
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main () {
-	char texto[101];
-	getline(texto)
+	string texto; 
+	int contador[26] = {0};
 	
-	for(  ){
-		
+	getline(cin, texto)
+	
+	for(int i=0; i<texto.length(); i++){
+			
 	}
 	
 	
