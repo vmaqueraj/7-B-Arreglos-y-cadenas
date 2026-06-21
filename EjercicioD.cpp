19f6e36fe57f5b66ff13515cfd80279c9c4a78cc
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-06-20 19:26:38 -0500

    Ejercicio D: Intento de estructura inicial, similar al ejercicio C

---
+++ EjercicioD.cpp
@@ -0,0 +1,23 @@
+// Ejercicio D Canicas
+
+#include <iostream>
+using namespace std;
+
+int main () {
+	long long n, c;
+	int a;
+	int A[1000005]={0};
+	
+	cin>>n >> c;
+	
+	for(int i=0; i<c; i++){
+		cin>> a;
+		A[a] = A[a]+1;
+	}
+	
+	for(int i=1; i<=c; i++){
+		cout << A[i] << endl; 
+	}
+	
+	return 0;
+}
