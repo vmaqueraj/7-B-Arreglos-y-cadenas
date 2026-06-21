78325a2e4a09e26c1aa5ceea7114ec0bd71a101e
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-06-21 12:37:38 -0500

    Ejercicio F: Intento de estructura inicial

---
+++ EjercicioF.cpp
@@ -0,0 +1,16 @@
+// Ejercicio F Ordenando las letras de la línea
+
+#include <iostream>
+using namespace std;
+
+int main () {
+	char texto[101];
+	getline(texto)
+	
+	for(  ){
+		
+	}
+	
+	
+	return 0;
+}
